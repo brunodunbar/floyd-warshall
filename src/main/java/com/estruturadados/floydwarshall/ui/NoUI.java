92f72557d1a5369c70663fa759f1ca93b874fdf4
Bruno Dunbar <brunodunbar@gmail.com>
2015-10-19 22:00:37 -0200

    Prototipo dos componentes de criação do grafo

--- src/main/java/com/estruturadados/floydwarshall/ui/NoUI.java
+++ src/main/java/com/estruturadados/floydwarshall/ui/NoUI.java
@@ -1,11 +1,16 @@
 package com.estruturadados.floydwarshall.ui;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 
 public class NoUI extends VBox {
+
+    @FXML
+    private Label label;
 
     public NoUI() {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/ui/NoUI.fxml"));
@@ -17,5 +22,9 @@ public class NoUI extends VBox {
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
+    }
+
+    public void setLabel(String value) {
+        label.setText(value);
     }
 }
