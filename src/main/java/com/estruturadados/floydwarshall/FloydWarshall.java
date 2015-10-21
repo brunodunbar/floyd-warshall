45466631ea95ce399da18f90ca1bb78595b2126a
Bruno Dunbar <brunodunbar@gmail.com>
2015-10-21 18:26:21 -0200

    Adicionado encoding UFT8 como padrão no pom

--- src/main/java/com/estruturadados/floydwarshall/FloydWarshall.java
+++ src/main/java/com/estruturadados/floydwarshall/FloydWarshall.java
@@ -1,28 +1,28 @@
 package com.estruturadados.floydwarshall;
 
-/*Definição
-O algoritmo de Floyd-Warshall recebe como entrada uma matriz de adjacência que 
+/*Definiï¿½ï¿½o
+O algoritmo de Floyd-Warshall recebe como entrada uma matriz de adjacï¿½ncia que 
 representa um grafo (V, E) orientado e valorado. O valor de um caminho entre dois 
-vértices é a soma dos valores de todas as arestas ao longo desse caminho. 
-As arestas E do grafo podem ter valores negativos, mas o grafo não pode conter 
-nenhum ciclo de valor negativo. O algoritmo calcula, para cada par de vértices, 
-o menor de todos os caminhos entre os vértices. Por exemplo, o caminho de menor custo.
-Sua ordem de complexidade é \theta(|V|^3).
+vï¿½rtices ï¿½ a soma dos valores de todas as arestas ao longo desse caminho. 
+As arestas E do grafo podem ter valores negativos, mas o grafo nï¿½o pode conter 
+nenhum ciclo de valor negativo. O algoritmo calcula, para cada par de vï¿½rtices, 
+o menor de todos os caminhos entre os vï¿½rtices. Por exemplo, o caminho de menor custo.
+Sua ordem de complexidade ï¿½ \theta(|V|^3).
 
 O algoritmo se baseia nos passos abaixo:
 
-Assumindo que os vértices de um grafo orientado G são V = {1,2,3,\ldots,n}, considere um subconjunto {1,2,3\ldots,k};
-Para qualquer par de vértices (i, j) em V, considere todos os caminhos de i a j cujos vértices intermédios pertencem ao subconjunto {1,2,3\ldots,k}, e p como o mais curto de todos eles;
-O algoritmo explora um relacionamento entre o caminho p e os caminhos mais curtos de i a j com todos os vértices intermédios em {1,2,3\ldots,k-1};
-O relacionamento depende de k ser ou não um vértice intermédio do caminho p.
-link da DEFINIÇÃO https://pt.wikipedia.org/wiki/Algoritmo_de_Floyd-Warshall
+Assumindo que os vï¿½rtices de um grafo orientado G sï¿½o V = {1,2,3,\ldots,n}, considere um subconjunto {1,2,3\ldots,k};
+Para qualquer par de vï¿½rtices (i, j) em V, considere todos os caminhos de i a j cujos vï¿½rtices intermï¿½dios pertencem ao subconjunto {1,2,3\ldots,k}, e p como o mais curto de todos eles;
+O algoritmo explora um relacionamento entre o caminho p e os caminhos mais curtos de i a j com todos os vï¿½rtices intermï¿½dios em {1,2,3\ldots,k-1};
+O relacionamento depende de k ser ou nï¿½o um vï¿½rtice intermï¿½dio do caminho p.
+link da DEFINIï¿½ï¿½O https://pt.wikipedia.org/wiki/Algoritmo_de_Floyd-Warshall
     ESPERO TER AJUDADO
 */
 
 public class FloydWarshall {
     static int[][]  P;
     static final int N = 4;
-    
+
     public static void main(String[] args) {
   //public static void Start() {
         int[][] M = {{0,5,999,999}, {50,0,15,5}, {30,99,0,15}, {15,99,5,0}};
@@ -33,19 +33,19 @@ public class FloydWarshall {
         printMatris(FloydAlgo(M));
         System.out.println("caminho da matris.");
         printMatris(P);
-    } 
+    }
     //link do algoritmo: https://www.youtube.com/watch?v=K6rI0umX-28 
-    //link explicação: https://www.youtube.com/watch?v=DfgaBkp02HY
+    //link explicaï¿½ï¿½o: https://www.youtube.com/watch?v=DfgaBkp02HY
     public static int[][] FloydAlgo(int [][] M){
         for (int k = 0; k < N; k++){
             for (int i = 0; i < N; i++){
                 for (int j = 0; j < N; j++){
                     // Para manter o controle
-                    if(M[i][k] + M[k][j] < M[i][j]){ // se for menor entao o menor caminho é M[i][k] + M[k][j]
+                    if(M[i][k] + M[k][j] < M[i][j]){ // se for menor entao o menor caminho ï¿½ M[i][k] + M[k][j]
                        M[i][j] = M[i][k] + M[k][j];
                        P[i][j] = k;
                     }
-                    // Retorna o menor caminho caso M[i][k] + M[k][j] não for <  M[i][j]
+                    // Retorna o menor caminho caso M[i][k] + M[k][j] nï¿½o for <  M[i][j]
                     M[i][j] = min(M[i][j], M[i][k] + M[k][j]);
                 }
             }
@@ -53,7 +53,7 @@ public class FloydWarshall {
     return M;
     }
     /*se o caminho i for maior que j, entao retorna j, caso contrario retorna i
-    isso serve para que o menor caminho seja gravado, diferente do dijkstra que grava 
+    isso serve para que o menor caminho seja gravado, diferente do dijkstra que grava
     o caminho maior*/
     public static int min(int i, int j){
         if(i > j){
@@ -62,18 +62,18 @@ public class FloydWarshall {
         return i;
     }
     public static void printMatris(int[][] Matris){
-        System.out.println("\n\t"); 
+        System.out.println("\n\t");
         for(int j = 0; j < N; j++){   // imprime os indices das colunas
-            
-            System.out.print("\t" + "Nó "+j );
+
+            System.out.print("\t" + "NÃ³ "+j );
         }
-        System.out.println();    
+        System.out.println();
         for(int j = 0; j < 35; j++){  //imprime  a linha tracejada
             System.out.print("-");
         }
         System.out.println();
         for(int i = 0; i < N; i++){   //Imprime a matriz inteira
-            System.out.print("Nó "+i + " |\t");
+            System.out.print("NÃ³ "+i + " |\t");
             for (int j = 0; j < N; j++) {
                 System.out.print(Matris[i][j]);
                 System.out.print("\t");
@@ -82,5 +82,5 @@ public class FloydWarshall {
         }
         System.out.println("\n");
     }
-    
+
 }
